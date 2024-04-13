a3e44ab0ba84f7a0ef124a23608632b62b640799
lindakwoo <lindakwoo@gmail.com>
2024-04-13 12:08:17 -0700

    newproducts screen

--- client/src/components/NewCard.jsx
+++ client/src/components/NewCard.jsx
@@ -1,12 +1,36 @@
-import React from "react";
-import { Box, Stack, styled } from "@mui/system";
+import React, { useState } from "react";
+import { Box, styled } from "@mui/system";
+import { Image } from "@chakra-ui/react";
 
-const NewCard = ({ height, backgroundColor }) => {
+const NewCard = ({ height, backgroundColor, image, image2 }) => {
+  const imageHeight = parseInt(height.replace("px", "")) - 50;
+  const [isShown, setIsShown] = useState(false);
+
   return (
     <Box
-      sx={{ height: { height }, backgroundColor: { backgroundColor }, width: "100%", my: "8px", borderRadius: "25px" }}
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: height,
+        backgroundColor: backgroundColor,
+        width: "100%",
+        my: "8px",
+        borderRadius: "25px",
+        "&:hover": {
+          transform: "scale(1.1)",
+          transitionDuration: "0.5s",
+        },
+      }}
     >
-      NewCard
+      <Image
+        sx={{ height: `${imageHeight}px`, width: "auto" }}
+        src={!isShown ? image : image2}
+        onMouseEnter={() => setIsShown(true)}
+        onMouseLeave={() => setIsShown(false)}
+        fallbackSrc='https://via.placeholder.com/150'
+        alt='tshirt'
+      />
     </Box>
   );
 };
--- client/src/screens/NewProducsScreen.jsx
+++ client/src/screens/NewProducsScreen.jsx
@@ -4,36 +4,121 @@ import NewCard from "../components/NewCard";
 
 const NewProducsScreen = () => {
   return (
-    <Stack alignItems='center' justifyContent='center'>
+    <Stack sx={{ mt: "24px" }} alignItems='center' justifyContent='center'>
       <Stack direction='row' gap={2} sx={{ flexWrap: "wrap" }}>
         <Stack direction='column' sx={{ width: "250px", height: "100%" }}>
           {" "}
-          <NewCard height='200px' backgroundColor='red' />
-          <NewCard height='350px' backgroundColor='blue' />
-          <NewCard height='175px' backgroundColor='yellow' />
-          <NewCard height='200px' backgroundColor='blue' />
+          <NewCard
+            height='200px'
+            backgroundColor='red'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='350px'
+            backgroundColor='blue'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='175px'
+            backgroundColor='yellow'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='200px'
+            backgroundColor='blue'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
         </Stack>
         <Stack direction='column' sx={{ width: "250px", height: "100%" }}>
           {" "}
-          <NewCard height='175px' backgroundColor='green' />
-          <NewCard height='250px' backgroundColor='yellow' />
-          <NewCard height='200px' backgroundColor='orange' />
-          <NewCard height='150px' backgroundColor='grey' />
-          <NewCard height='175px' backgroundColor='aqua' />
+          <NewCard
+            height='175px'
+            backgroundColor='green'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='250px'
+            backgroundColor='yellow'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='200px'
+            backgroundColor='orange'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='150px'
+            backgroundColor='grey'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='175px'
+            backgroundColor='aqua'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
         </Stack>
         <Stack direction='column' sx={{ width: "250px", height: "100%" }}>
           {" "}
-          <NewCard height='200px' backgroundColor='aqua' />
-          <NewCard height='250px' backgroundColor='purple' />
-          <NewCard height='175px' backgroundColor='lime' />
-          <NewCard height='300px' backgroundColor='pink' />
+          <NewCard
+            height='200px'
+            backgroundColor='aqua'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='250px'
+            backgroundColor='purple'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='175px'
+            backgroundColor='lime'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='300px'
+            backgroundColor='pink'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
         </Stack>
         <Stack direction='column' sx={{ width: "250px", height: "100%" }}>
           {" "}
-          <NewCard height='300px' backgroundColor='orange' />
-          <NewCard height='175px' backgroundColor='pink' />
-          <NewCard height='175px' backgroundColor='yellow' />
-          <NewCard height='250px' backgroundColor='blue' />
+          <NewCard
+            height='300px'
+            backgroundColor='orange'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='175px'
+            backgroundColor='pink'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='175px'
+            backgroundColor='yellow'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
+          <NewCard
+            height='250px'
+            backgroundColor='blue'
+            image='/images/rabbits.png'
+            image2='/images/yellow_rabbits.png'
+          />
         </Stack>
       </Stack>
     </Stack>
